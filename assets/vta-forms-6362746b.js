28f787319f4526b4708a40803be9a158ff03ea68
dawidroguziak@gmail.com <dawidroguziak@gmail.com>
2024-03-05 21:29:33 +0100

    feat: add dist

---
+++ assets/vta-forms-6362746b.js
@@ -0,0 +1,1 @@
+const t=[{id:1,title:"Test title 1",content:"kot ma ale"},{id:2,title:"Test title 2",content:"ala ma kota"},{id:3,title:"Test title 3",content:"lorem ipsum"},{id:4,title:"Test title 4",content:"sen"}];export{t as v};
